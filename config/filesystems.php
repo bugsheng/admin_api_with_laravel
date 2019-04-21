300e61d23e0efe4f5b6ae839900b010e580bfce5
shenglin-php <s617790418@126.com>
2019-04-22 05:42:52 +0800

    完善文件系统-添加阿里云Oss文件操作相关

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,17 +64,17 @@ return [
             'url' => env('AWS_URL'),
         ],
 
+        //阿里Oss配置
         'ali_oss' => [
             'driver' => 'ali_oss',
-            'access_key_id' => 'LTAIPjFI53jdaPWP',//从OSS获得的AccessKeyId
-            'access_key_secret' => 'P5BDI1w0vhhIRTQzeXFw8h9BfY6BoP',//从OSS获得的AccessKeySecret
-            'bucket' => 'bugsheng-dev',//OSS中设置的空间bucket
-            'endpoint' => 'oss-cn-hangzhou.aliyuncs.com',//您选定的OSS数据中心访问域名，例如oss-cn-hangzhou.aliyuncs.com
-            'isCName' => true,//<true|false>是否对Bucket做了域名绑定，并且Endpoint参数填写的是自己的域名
-            'cdn_domain' => 'oss-bugsheng-dev.bugsheng.com',// 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
-            'endpoint_internal' => 'oss-cn-hangzhou-internal.aliyuncs.com',//内网地址
-            'ssl' => false,//<true|false>是否使用ssl 即链接是否使用https
-
+            'access_key_id' => env('ALI_OSS_ACCESS_KEY_ID'),//从OSS获得的AccessKeyId
+            'access_key_secret' => env('ALI_OSS_ACCESS_KEY_SECRET'),//从OSS获得的AccessKeySecret
+            'bucket' => env('ALI_OSS_BUCKET'),//OSS中设置的空间bucket
+            'cdn_domain' => env('ALI_OSS_CDN_DOMAIN'),// 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
+            'endpoint' => env('ALI_OSS_ENDPOINT'),//您选定的OSS数据中心访问域名，例如oss-cn-hangzhou.aliyuncs.com
+            'endpoint_internal' => env('ALI_OSS_ENDPOINT_INTERNAL'),//内网地址
+            'isCName' => env('ALI_OSS_IS_CNAME', false),//<true|false>是否对Bucket做了域名绑定，并且Endpoint参数填写的是自己的域名
+            'ssl' => env('ALI_OSS_SSL', false),//<true|false>是否使用ssl 即链接是否使用https
         ]
     ],
 
